b10180eb31d7f175b1da369648b291556165ec49
sean-code <john.nganga@student.moringaschool.com>
2022-12-14 18:32:12 +0300

    Change colors

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 
-// Dropdown Menu
+// // Dropdown Menu
 
 
 
 
-// Profile Card
-var messageBox = document.querySelector('.js-message');
-  var btn = document.querySelector('.js-message-btn');
-  var card = document.querySelector('.js-profile-card');
-  var closeBtn = document.querySelectorAll('.js-message-close');
+// // Profile Card
+// var messageBox = document.querySelector('.js-message');
+//   var btn = document.querySelector('.js-message-btn');
+//   var card = document.querySelector('.js-profile-card');
+//   var closeBtn = document.querySelectorAll('.js-message-close');
 
-  btn.addEventListener('click',function (e) {
-      e.preventDefault();
-      card.classList.add('active');
-  });
+//   btn.addEventListener('click',function (e) {
+//       e.preventDefault();
+//       card.classList.add('active');
+//   });
 
-  closeBtn.forEach(function (element, index) {
-     console.log(element);
-      element.addEventListener('click',function (e) {
-          e.preventDefault();
-          card.classList.remove('active');
-      });
-  });
+//   closeBtn.forEach(function (element, index) {
+//      console.log(element);
+//       element.addEventListener('click',function (e) {
+//           e.preventDefault();
+//           card.classList.remove('active');
+//       });
+//   });
 
 
   
+
